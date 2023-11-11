acca41ad6c3af78b87a23e5e54ab74f3205a9749
Gurutharshan Mahendiran <62455117+guru06m@users.noreply.github.com>
2023-11-11 18:07:51 +0530

    updated

--- user-service/src/main/java/com/example/userservice/controler/UserController.java
+++ user-service/src/main/java/com/example/userservice/controler/UserController.java
@@ -17,6 +17,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ProblemDetail;
